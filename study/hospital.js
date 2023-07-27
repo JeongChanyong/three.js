d23aecb2c2318021b9a095c1108d0db075366262
jeongchanyong <chanyong.jeong@kakao.com>
2023-07-27 14:38:13 +0900

    glb 파일 제외

--- study/hospital.js
+++ study/hospital.js
@@ -1,12 +1,16 @@
 import * as THREE from "../build/three.module.js";
+import { GLTFLoader } from "../examples/jsm/loaders/GLTFLoader.js";
 import { OrbitControls } from "../examples/jsm/controls/OrbitControls.js";
+import { FirstPersonControls } from "../examples/jsm/controls/FirstPersonControls.js";
+import { PointerLockControls } from "../examples/jsm/controls/PointerLockControls.js";
 
 // 윈도우가 로드 되면 App Class 실행
+
 class App {
 	constructor() {
 		// _ 시작하는 경우 자바로 비교하면 privite App Class 내에서만 사용 해야 한다.
-
 		const divContainer = document.querySelector("#webgl-container");
+
 		// 다른 method에서 참조 하기 위해 필드 설정
 		this._divContainer = divContainer;
 
@@ -35,86 +39,41 @@ class App {
 		this._scene.add(axes);
 	}
 
-	_setupControls() {
-		new OrbitControls(this._camera, this._divContainer);
-	}
-
 	_setupCamera() {
 		const width = this._divContainer.clientWidth;
 		const height = this._divContainer.clientHeight;
 		const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
-
-		// camera.position.z = 10;
-		camera.position.set(7, 7, 0);
+		camera.position.z = 50;
+		// camera.position.set(0, -10, -5);
 		// camera.lookAt(0, 0, 0);
-
 		this._camera = camera;
 	}
 
 	_setupLight() {
-		// 주변광
-		const light = new THREE.AmbientLight(0xffffff, 5);
-		this._scene.add(light);
-		this._light = light;
+		const color = 0xffffff; // 광원의 색상
+		const intensity = 1; // 광원의 세기
+		const light = new THREE.AmbientLight(color, intensity);
+		light.position.set(-1, 2, 4); // 광원의 위치
+		this._scene.add(light); // 광원 추가
 	}
 
 	_setupModel() {
-		const groundGeometry = new THREE.PlaneGeometry(10, 10);
-		const groundMaterial = new THREE.MeshStandardMaterial({
-			color: "#2c3e50",
-			roughness: 0.5,
-			metalness: 0.5,
-			side: THREE.DoubleSide,
+		const url = "../model/test.glb";
+		const loader = new GLTFLoader();
+
+		loader.load(url, (object) => {
+			console.log(object);
+			const root = object.scene;
+			this._scene.add(root);
 		});
+		undefined,
+			function (error) {
+				console.error(error);
+			};
+	}
 
-		const groud = new THREE.Mesh(groundGeometry, groundMaterial);
-		groud.rotation.x = THREE.MathUtils.degToRad(-90);
-		this._scene.add(groud);
-
-		const bigSphereGeometry = new THREE.SphereGeometry(1.5, 64, 64, 0, Math.PI);
-		const bigSphereMaterial = new THREE.MeshStandardMaterial({
-			color: "#ffffff",
-			roughness: 0.1,
-			metalness: 0.2,
-		});
-
-		const bigSphere = new THREE.Mesh(bigSphereGeometry, bigSphereMaterial);
-		bigSphere.rotation.x = THREE.MathUtils.degToRad(-90);
-		this._scene.add(bigSphere);
-
-		const torusGeometry = new THREE.TorusGeometry(0.4, 0.1, 32, 32);
-		const torusMaterial = new THREE.MeshStandardMaterial({
-			color: "#9B59b6",
-			roughness: 0.5,
-			metalness: 0.9,
-		});
-
-		for (let i = 0; i < 8; i++) {
-			const torusPivot = new THREE.Object3D();
-			const torus = new THREE.Mesh(torusGeometry, torusMaterial);
-			torusPivot.rotation.y = THREE.MathUtils.degToRad(45 * i);
-			torus.position.set(3, 0.5, 0);
-			torusPivot.add(torus);
-			this._scene.add(torusPivot);
-		}
-
-		const smallSphereGeometry = new THREE.SphereGeometry(0.3, 32, 32);
-		const smallSphereMaterial = new THREE.MeshStandardMaterial({
-			color: "#e74c3c",
-			roughness: 0.2,
-			metalness: 0.5,
-		});
-
-		const smallSpherePivot = new THREE.Object3D();
-		const smallSphere = new THREE.Mesh(
-			smallSphereGeometry,
-			smallSphereMaterial
-		);
-
-		smallSpherePivot.add(smallSphere);
-		smallSpherePivot.name = "smallSpherePivot";
-		smallSphere.position.set(3, 0.5, 0);
-		this._scene.add(smallSpherePivot);
+	_setupControls() {
+		new OrbitControls(this._camera, this._divContainer);
 	}
 
 	// 창 크기 변경시 마다 크기를 조정
